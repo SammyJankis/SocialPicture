56c11faff8152b066a0fc513b60f733899763618
Arturo Guillen <arturo.guillen@privalia.com>
2017-09-04 17:22:20 +0200

    Added Tweet class

---
+++ app/src/main/java/com/arturoguillen/socialpicture/entities/client/twitter/Tweet.java
@@ -0,0 +1,14 @@
+package com.arturoguillen.socialpicture.entities.client.twitter;
+
+import java.io.Serializable;
+
+/**
+ * Created by arturo.guillen on 04/09/2017.
+ */
+
+public class Tweet implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+
+}
